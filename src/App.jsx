a048154bede030b2b76725121ae8f91682e4cae8
minb__m <kmb3322@naver.com>
2025-05-27 14:14:41 +0900

    메인화면 리폼

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,10 @@
 // src/App.jsx
 import { Box, ChakraProvider } from '@chakra-ui/react';
-import React from 'react';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 //import EndScreen from './components/EndScreen';
 //import HomeScreen from './components/HomeScreen';
-import HomeScreen from './components/HomeScreen';
+import StartHomePage from './components/StartHomePage';
+import TicketScreen from './components/ticketScreen';
 import theme from './theme'; // 커스텀 테마 임포트
 
 function App() {
@@ -16,7 +17,13 @@ function App() {
         justifyContent="center"
         alignItems="center"
       >
-        <HomeScreen />
+        <BrowserRouter>
+          <Routes>
+            <Route path='/'        element={<StartHomePage />} />
+            <Route path='/ticket'  element={<TicketScreen />} />
+            
+          </Routes>
+      </BrowserRouter>
       </Box>
     </ChakraProvider>
   );
