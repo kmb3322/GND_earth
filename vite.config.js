489545dcaaa6e74bb80e39a509c43ee8ce71ef0d
minb__m <kmb3322@naver.com>
2025-06-06 16:49:58 +0900

    server error

--- vite.config.js
+++ vite.config.js
@@ -6,4 +6,13 @@ export default defineConfig({
   optimizeDeps: {
     include: ['@chakra-ui/react', '@chakra-ui/icons', '@emotion/react', '@emotion/styled', 'framer-motion'],
   },
+  server: {
+    proxy: {
+      '/api': {
+        target: 'http://localhost:3000', // vercel dev 가 기본으로 3000
+        changeOrigin: true,
+        secure: false,
+      },
+    },
+  },
 });
